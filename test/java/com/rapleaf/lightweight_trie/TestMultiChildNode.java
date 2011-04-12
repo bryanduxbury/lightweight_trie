4dfa6290b4063774009b7ba7a78443a58552d03a
bryanduxbury <bryan@rapleaf.com>
2011-04-11 19:23:04 -0700

    tests for MultiChildNode

--- test/java/com/rapleaf/lightweight_trie/TestMultiChildNode.java
+++ test/java/com/rapleaf/lightweight_trie/TestMultiChildNode.java
@@ -4,6 +4,16 @@ import junit.framework.TestCase;
 
 public class TestMultiChildNode extends TestCase {
   public void testIt() throws Exception {
-    fail();
+    Object value1 = new Object();
+    LeafNode l1 = new LeafNode("ab".toCharArray(), value1);
+    Object value2 = new Object();
+    LeafNode l2 = new LeafNode("cd".toCharArray(), value2);
+    MultiChildNode n = new MultiChildNode("".toCharArray(), null, new AbstractNode[]{l1, l2});
+
+    assertNull(n.get("a".toCharArray(), 0));
+    assertNull(n.get("abc".toCharArray(), 0));
+    assertNull(n.get("fff".toCharArray(), 0));
+    assertEquals(value1, n.get("ab".toCharArray(), 0));
+    assertEquals(value2, n.get("cd".toCharArray(), 0));
   }
 }
