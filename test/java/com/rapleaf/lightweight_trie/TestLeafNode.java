389fd97aa2437f8b125040e140d6afc02d5f6519
bryanduxbury <bryan@rapleaf.com>
2011-04-11 18:40:14 -0700

    don't need this test.

--- test/java/com/rapleaf/lightweight_trie/TestLeafNode.java
+++
@@ -1,9 +0,0 @@
-package com.rapleaf.lightweight_trie;
-
-import junit.framework.TestCase;
-
-public class TestLeafNode extends TestCase {
-  public void testIt() throws Exception {
-    fail();
-  }
-}
