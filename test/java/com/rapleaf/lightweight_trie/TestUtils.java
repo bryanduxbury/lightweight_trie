c203f2883e38a91adb8b5d2e4e6335eb535723a4
bryanduxbury <bryan@rapleaf.com>
2011-04-11 19:33:22 -0700

    some tests for getCommonLength

--- test/java/com/rapleaf/lightweight_trie/TestUtils.java
+++ test/java/com/rapleaf/lightweight_trie/TestUtils.java
@@ -4,6 +4,9 @@ import junit.framework.TestCase;
 
 public class TestUtils extends TestCase {
   public void testIt() throws Exception {
-    fail();
+    assertEquals(0, Utils.getCommonLength("abc".toCharArray(), 0, "bcd".toCharArray()));
+    assertEquals(2, Utils.getCommonLength("abc".toCharArray(), 1, "bcd".toCharArray()));
+    assertEquals(2, Utils.getCommonLength("abc".toCharArray(), 1, "bc".toCharArray()));
+    assertEquals(0, Utils.getCommonLength("abc".toCharArray(), 0, "bc".toCharArray()));
   }
 }
